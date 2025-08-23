657d2e776367127523ecee66cfd5a9705a6307bc
sbasile-ch <sbasile@companieshouse.gov.uk>
2025-08-23 13:14:12 +0100

    refresh tasks

--- groups/infrastructure/locals.tf
+++ groups/infrastructure/locals.tf
@@ -66,6 +66,7 @@ locals {
 
   # TASK ENVIRONMENT: GLOBAL SECRET Version + SERVICE SECRET Version
   task_environment = concat(local.ssm_global_version_map, local.ssm_service_version_map, [
+    { name : "DUMMY_VALUE", value : "1" },
     { name : "WEBLATE_DEBUG", value : "1" },
     { name : "WEBLATE_LOGLEVEL", value : "DEBUG" },
     { name : "POSTGRES_HOST", value : aws_db_instance.weblate.address },
