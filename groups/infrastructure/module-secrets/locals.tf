1d62748dc39c77371f4b89998cf5c03276c13bdd
sbasile-ch <sbasile@companieshouse.gov.uk>
2025-10-10 22:17:37 +0100

    typo

--- groups/infrastructure/module-secrets/locals.tf
+++ groups/infrastructure/module-secrets/locals.tf
@@ -4,7 +4,7 @@ locals {
   service_secrets = jsondecode(data.vault_generic_secret.service_secrets.data_json)
 
   service_secrets_sanitised = {
-    for k, v in module.common_secrets.service_secrets :
+    for k, v in local.service_secrets :
     k => v if !contains([
       "psql_master_user",
       "psql_master_password",
