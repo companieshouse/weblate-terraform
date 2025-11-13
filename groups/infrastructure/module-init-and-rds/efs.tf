7a9f4bd2b09cf13f63b0efd736b5d59de165c86d
sbasile-ch <sbasile@companieshouse.gov.uk>
2025-11-13 08:20:33 +0000

    use config var

--- groups/infrastructure/module-init-and-rds/efs.tf
+++ groups/infrastructure/module-init-and-rds/efs.tf
@@ -28,7 +28,7 @@ resource "aws_efs_access_point" "weblate_accp" {
 
 resource "aws_security_group" "efs" {
   name        = var.config.efs_sg_name
-  vpc_id      = data.aws_vpc.vpc.id
+  vpc_id      = var.config.vpc_id
   description = "EFS security group"
 }
 
