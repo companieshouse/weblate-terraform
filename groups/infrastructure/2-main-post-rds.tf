9586472734de45796ce8c3010567677f06e4cfb0
sbasile-ch <sbasile@companieshouse.gov.uk>
2025-11-14 15:02:29 +0000

    rem8

--- groups/infrastructure/2-main-post-rds.tf
+++ groups/infrastructure/2-main-post-rds.tf
@@ -20,44 +20,44 @@ module "secrets" {
 
 
 # run 1st: celery-beat only (which should start before the other ECS services)
-module "ecs-service-celery-beat" {
-  source = "./module-ecs"
+# module "ecs-service-celery-beat" {
+#   source = "./module-ecs"
 
-  # the loop will process only 1 iteration (celery-beat)
-  for_each = {
-    for name, cfg in local.ecs_service_configs :
-    name => cfg
-    if cfg.service_name == "weblate-celery-beat"
-  }
+#   # the loop will process only 1 iteration (celery-beat)
+#   for_each = {
+#     for name, cfg in local.ecs_service_configs :
+#     name => cfg
+#     if cfg.service_name == "weblate-celery-beat"
+#   }
 
-  config                  = each.value
-  # rds_security_group_id   = data.aws_security_group.rds_sg.id   // prev. phase
-  # redis_security_group_id = data.aws_security_group.redis_sg.id // prev. phase
-  efs_security_group_id   = aws_security_group.efs.id           // this phase
+#   config                  = each.value
+#   # rds_security_group_id   = data.aws_security_group.rds_sg.id   // prev. phase
+#   # redis_security_group_id = data.aws_security_group.redis_sg.id // prev. phase
+#   efs_security_group_id   = aws_security_group.efs.id           // this phase
 
-  depends_on = [
-    module.secrets,
-    module.db_config,
-    aws_efs_mount_target.weblate
-  ]
-}
+#   depends_on = [
+#     module.secrets,
+#     module.db_config,
+#     aws_efs_mount_target.weblate
+#   ]
+# }
 
 
 # run 2nd: all others ECS
-module "ecs-services" {
-  source = "./module-ecs"
+# module "ecs-services" {
+#   source = "./module-ecs"
 
-  # the loop will process all except 1 (celery-beat)
-  for_each = {
-    for name, cfg in local.ecs_service_configs :
-    name => cfg
-    if cfg.service_name != "weblate-celery-beat"
-  }
+#   # the loop will process all except 1 (celery-beat)
+#   for_each = {
+#     for name, cfg in local.ecs_service_configs :
+#     name => cfg
+#     if cfg.service_name != "weblate-celery-beat"
+#   }
 
-  config                  = each.value
-  # rds_security_group_id   = data.aws_security_group.rds_sg.id   // prev. phase
-  # redis_security_group_id = data.aws_security_group.redis_sg.id // prev. phase
-  efs_security_group_id   = aws_security_group.efs.id           // this phase
+#   config                  = each.value
+#   # rds_security_group_id   = data.aws_security_group.rds_sg.id   // prev. phase
+#   # redis_security_group_id = data.aws_security_group.redis_sg.id // prev. phase
+#   efs_security_group_id   = aws_security_group.efs.id           // this phase
 
-  depends_on = [module.ecs-service-celery-beat]  # <-- here the dependency which will run this after
-}
+#   depends_on = [module.ecs-service-celery-beat]  # <-- here the dependency which will run this after
+# }
